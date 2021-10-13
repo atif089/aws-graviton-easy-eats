8f682ce468424f93bc4061879eb08d1a049be1be
Atif Mohammed <atif089@gmail.com>
2021-10-13 17:56:14 +0200

    dockerize part-2

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  NOTION_ACCESS_TOKEN: 'secret_bikZkCgvJzzgeXy9hHDHlwhSShspx1NH68yDFjjt2am',
-  NOTION_PARENT_ID: '4c234d52-f60f-40f0-98fe-ee51bc21f0fa',
+  NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN,
+  NOTION_PARENT_ID: process.env.NOTION_PARENT_ID
 }
